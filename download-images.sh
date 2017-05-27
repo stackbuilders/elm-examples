f1e5db4f9a4e62f2776a539739339ac8fc03fe6b
CristhianMotoche <cristhian.motoche@gmail.com>
2017-05-26 23:35:14 -0500

    Update download images script

--- download-images.sh
+++ download-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(seq 0 20); do
-  wget "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"$i".png"
+  wget "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$i.png" -O "src/Cmd/$i.png"
 done
